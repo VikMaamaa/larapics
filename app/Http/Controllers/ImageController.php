77d0327407112c6a7bb40eb73c65805c511cccef
vikmaamaa <maamaavictor@yahoo.com>
2023-04-03 22:45:07 +0100

    feat: create image upload form

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ImageRequest;
 use App\Models\Image;
 use Illuminate\Http\Request;
 
@@ -17,5 +18,14 @@ class ImageController extends Controller
 
     public function show(Image $image) {
         return view('image.show', compact('image'));
+    }
+
+    public function create() {
+        return view('image.create');
+    }
+
+    public function store(ImageRequest $request)
+    {
+        dd($request->validated());
     }
 }
