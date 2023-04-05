ab026c09ded0f75948715a7408c2f7f41e1e0100
vikmaamaa <maamaavictor@yahoo.com>
2023-04-05 02:29:21 +0100

    feat: refactor edit images

--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -1,9 +1,44 @@
-<h1>Edit image</h1>
+<x-layout title="Update Image">
+    <div class="container-fluid mt-4">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-header">Update Image</div>
+
+                    <div class="card-body">
+                        <x-form action="{{ $image->route('update') }}" method="PUT" >
+
+                            <div class="mb-4">
+                                <img src="{{ $image->fileUrl()}}" alt="{{$image->title}}" class="image-fluid w-100">
+                            </div>
+                            <div class="mb-4">
+                                <label class="form-label" for="title">Title</label>
+                                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" value="{{ old('title', $image->title) }}">
+                                @error('title')
+                                    <div>{{$message}}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                                <a href="{{ route('images.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                            </div>
+                        </x-form>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 
-<x-form action="{{ $image->route('update') }}" method="PUT" >
-    {{-- @csrf
-    @method('PUT') --}}
+</x-layout>
+
+
+
+{{-- <x-form action="{{ $image->route('update') }}" method="PUT" >
+
     <div>
         <img src="{{ $image->fileUrl()}}" alt="{{$image->title}}" width="400">
     </div>
@@ -23,4 +58,4 @@
     </div>
 
     <button type="submit">Update</button>
-</x-form>
+</x-form> --}}
